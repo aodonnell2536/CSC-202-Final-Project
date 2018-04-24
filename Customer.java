84ba5720fbe731b904ba14e77da02f69779ce299
ato2205198 <38472098+ato2205198@users.noreply.github.com>
2018-04-24 14:33:32 -0400

    Added more analytic fields for statistics

--- Customer.java
+++ Customer.java
@@ -3,6 +3,8 @@ public class Customer implements Comparable<Customer> {
 
 	private int arrivalTime, serviceTime, finishTime, waitTime;
 	private int id;
+	private boolean full;		// True for FULL, false for SELF
+	private int laneNumber;
 
 	public Customer() {
 
@@ -18,12 +20,16 @@ public class Customer implements Comparable<Customer> {
 	public int getFinishTime() { return finishTime; }
 	public int getWaitTime() { return waitTime; }
 	public int getID() { return id; }
+	public boolean isFull() { return full; }
+	public int getLaneNumber() { return laneNumber; }
 
 	public void setArrivalTime(int arrivalTime) { this.arrivalTime = arrivalTime; }
 	public void setServiceTime(int serviceTime) { this.serviceTime = serviceTime; }
 	public void setFinishTime(int finishTime) { this.finishTime = finishTime; }
 	public void setWaitTime() { waitTime = finishTime - serviceTime - arrivalTime; }
 	public void setID(int id) { this.id = id; }
+	public void setFull(boolean full) { this.full = full; }
+	public void setLaneNumber(int laneNumber) { this.laneNumber = laneNumber; }
 	
 	// Allows customers to be sorted in order of id
 	public int compareTo(Customer customer) {
