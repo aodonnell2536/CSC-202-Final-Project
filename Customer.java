4d069e60bdead0a92e4c9352b587bae62b218e19
ato2205198 <38472098+ato2205198@users.noreply.github.com>
2018-04-24 12:30:57 -0400

    Added ID field to customer for logging

--- Customer.java
+++ Customer.java
@@ -2,24 +2,27 @@
 public class Customer {
 
 	private int arrivalTime, serviceTime, finishTime, waitTime;
-	
+	private int id;
+
 	public Customer() {
-		
+
 	}
-	
+
 	public Customer(int arrivalTime, int serviceTime) {
 		this.arrivalTime = arrivalTime;
 		this.serviceTime = serviceTime;
 	}
-	
+
 	public int getArrivalTime() { return arrivalTime; }
 	public int getServiceTime() { return serviceTime; }
 	public int getFinishTime() { return finishTime; }
 	public int getWaitTime() { return waitTime; }
-	
+	public int getID() { return id; }
+
 	public void setArrivalTime(int arrivalTime) { this.arrivalTime = arrivalTime; }
 	public void setServiceTime(int serviceTime) { this.serviceTime = serviceTime; }
 	public void setFinishTime(int finishTime) { this.finishTime = finishTime; }
 	public void setWaitTime() { waitTime = serviceTime - arrivalTime; }
-	
+	public void setID(int id) { this.id = id; }
+
 }
