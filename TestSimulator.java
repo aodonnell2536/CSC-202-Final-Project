616321fe3ab05f3a4e146a70671c389f6cc17266
ato2205198 <38472098+ato2205198@users.noreply.github.com>
2018-04-25 10:43:57 -0400

    Added functionality

--- TestSimulator.java
+++ TestSimulator.java
@@ -1,17 +1,71 @@
+import java.awt.FileDialog;
+import java.awt.Frame;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.TreeSet;
 
 public class TestSimulator extends Simulator {
 
 	// TODO : Create the content for the TestSimulator class
 	
-	public TestSimulator() {
-		
-	}
+	public TestSimulator() { initialize(); }
 	
 	protected void initialize() {
 		
+		time = 0;
+		currentID = 1;
+		customersHelped = 0;
+		customersDissatisfied = 0;
+		customersSatisfied = 0;
+		averageWaitTime = 0;
+		totalUnusedTime = 0;
+		customers =  new Queue<Customer>();
+		fullLaneQueues = new ArrayList<Queue<Customer>>();
+		selfLaneQueue = new Queue<Customer>();
+		customersCompleted = new TreeSet<Customer>();
+		
+		try { readData(); } 
+		catch (IOException e) { e.printStackTrace(); }
+		
+		fullLaneUnusedTime = new int[numFullLanes];
+		selfLaneUnusedTime = new int[numSelfLanes];
+		selfLanes = new Customer[numSelfLanes];
+		for (int i = 0; i < numFullLanes; i++)
+			fullLaneQueues.add(new Queue<Customer>());
+		
 	}
 	
-	private void readData() {
+	private void readData() throws IOException {
+	
+		System.out.println("Please select the file to read the test data from.");
+		Frame f = new Frame();
+		f.setVisible(true);
+		FileDialog fd = new FileDialog(f);
+		fd.setVisible(true);
+		File file = new File(fd.getDirectory() + fd.getFile());
+		f.setVisible(false);
+		
+		
+		Scanner in = new Scanner(file);
+		
+		numFullLanes = in.nextInt();
+		numSelfLanes = in.nextInt();
+		selfPercentSlower = in.nextInt();
+		
+		numCustomers = in.nextInt();
+		for (int i = 0; i < numCustomers; i++) {
+			int arrival = in.nextInt();
+			int service = in.nextInt();
+			boolean full = in.nextBoolean();
+			Customer customer = new Customer(arrival, service);
+			customer.setFull(full);
+			customers.push(customer);
+		}
+		
+		in.close();
+		
 		
 	}
 	
